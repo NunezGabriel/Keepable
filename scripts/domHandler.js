b5022d9b9d8ace9a45ab70c1abc337f9e35b02f9
Alex Mendoza <alex.mendoza.et@gmail.com>
2023-05-26 18:58:43 -0500

    avanced of action event for notes

--- scripts/domHandler.js
+++ scripts/domHandler.js
@@ -21,14 +21,14 @@ const CreateTitle = (title) => {
 
 const CreateInput = () => {
   const temmplate = `
-        <section class = 'inputBox'>
-            <input class = 'title' type="text" placeholder="Title">
-            <input class = 'bodyText' type="textarea" placeholder="Take a note...">
-            <div class = 'inputActions'>
-                <div class = 'iconPaletArea'>
+        <section class="inputBox">
+            <input class="title" type="text" placeholder="Title">
+            <input class="bodyText" type="textarea" placeholder="Take a note...">
+            <div class="inputActions">
+                <div class="iconPaletArea">
                     <img src="Assests/images/palet-icon.svg">
                 </div>
-                <p class= 'keepit' style = 'color: black;'>Keep it!</p>
+                <p class="keepit" style="color: black;">Keep it!</p>
             </div>
         </section>
     `;
@@ -39,19 +39,38 @@ const CreateInput = () => {
   };
 };
 
-const keepitAction = () =>{
-    const keepit = document.querySelector('.keepit')
-    const title = document.querySelector('.title')
-    const bodyText = document.querySelector('.bodyText')
-    const notesContainer = document.querySelector('.notesContainer')
-    keepit.addEventListener('click',()=>{
-        let CreateCard = document.createElement('DIV')
-        CreateCard.setAttribute('class', 'card')
-        CreateCard.innerHTML = 
-        `
+const actionEvent = () => {
+  const palets = document.querySelectorAll(".iconPaletArea");
+  let activador = false;
+
+  const clickHandler = () => {
+    if (!activador) {
+      console.log("hola");
+      activador = true;
+    } else {
+      console.log("adios");
+      activador = false;
+    }
+  };
+
+  palets.forEach((palet) => {
+    palet.removeEventListener("click", clickHandler);
+    palet.addEventListener("click", clickHandler);
+  });
+};
+
+const keepitAction = () => {
+  const keepit = document.querySelector(".keepit");
+  const title = document.querySelector(".title");
+  const bodyText = document.querySelector(".bodyText");
+  const notesContainer = document.querySelector(".notesContainer");
+  keepit.addEventListener("click", () => {
+    let CreateCard = document.createElement("DIV");
+    CreateCard.setAttribute("class", "card");
+    CreateCard.innerHTML = `
                 <h2>${title.value}</h2>
                 <p>${bodyText.value}</p>
-                <div class = 'actionsNotes'>
+                <div class="actionsNotes">
                     <div class="iconPaletArea iPA">
                         <img src="Assests/images/palet-icon.svg" alt="">
                     </div>
@@ -59,29 +78,14 @@ const keepitAction = () =>{
                         <img src="Assests/images/trash.svg" alt="">
                     </div>
                 </div>
-        `
-        notesContainer.appendChild(CreateCard)
-    })
-}
+        `;
+    notesContainer.appendChild(CreateCard);
 
-const actionEvent = () =>{
-    const palet = document.querySelector('.iconPaletArea')
-    let activador = false
-    palet.addEventListener('click', ()=>{
-        if(!activador){
-            console.log('hola')
-            activador = true
-        }
-        else{
-            console.log('adios')
-            activador = false
-        }
-    })
-}
-
-
-const CreateCard = () => {
-
+    // Volver a llamar a actionEvent para agregar event listeners a los nuevos elementos
+    actionEvent();
+  });
 };
+
+const CreateCard = () => {};
 
 const addListeners = () => {};
