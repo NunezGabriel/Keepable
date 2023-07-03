a7e7993274f96b0539a27f7adf0fb116e4f521eb
Gabriel <emilio.nunez.arenas@gmail.com>
2023-07-03 18:37:16 -0500

    LAst change to repo

--- scripts/index.js
+++ scripts/index.js
@@ -1,73 +1,11 @@
-const Main = DOMHandler(".inputContainer");
-const inputWhite = CreateInput();
-Main.load(inputWhite.render());
+let App = DOMHandler("#main");
+let mainView = Layout();
+App.load(mainView);
 
-const formInputs = document.querySelector(".inputBox");
-const formBox = document.querySelector(".inputActions");
-const notesContainer = document.querySelector(".notesContainer");
-const trastView = document.querySelector(".trash");
-const notesView = document.querySelector(".notes");
+let Sidebar = DOMHandler(".aside");
+Sidebar.load(sidebarRender());
 
-(titleTag = formInputs.querySelector("input")),
-  (descTag = formInputs.querySelector("textarea")),
-  (addBtn = formBox.querySelector("button"));
+let Cards = DOMHandler(".card-section");
+Cards.load(CardsView());
 
-const notes = JSON.parse(localStorage.getItem("notes") || "[]");
-const trash = JSON.parse(localStorage.getItem("trash") || "[]");
-
-
-showNotes();
-
-function showPalet(elem) {
-  elem.parentElement.children[0].classList.toggle("show");
-}
-
-function trashNote(noteId) {
-  var deletedNote = notes.splice(noteId, 1)[0];
-
-  trash.push(deletedNote);
-
-  localStorage.setItem("notes", JSON.stringify(notes));
-  localStorage.setItem("trash", JSON.stringify(trash));
-  showNotes();
-}
-
-function deleteNote(trashId) {
-  trash.splice(trashId, 1);
-  localStorage.setItem("trash", JSON.stringify(trash));
-  showTrash();
-}
-
-function restoreNote(trashId) {
-  var restoreNote = trash.splice(trashId, 1)[0];
-
-  notes.push(restoreNote);
-  localStorage.setItem("trash", JSON.stringify(trash));
-  localStorage.setItem("notes", JSON.stringify(notes));
-  showTrash();
-}
-
-trastView.addEventListener("click", (e) => {
-  showTrash();
-});
-
-notesView.addEventListener("click", (e) => {
-  document.querySelectorAll(".card").forEach((note) => note.remove());
-  showNotes();
-});
-
-addBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  let noteTitle = titleTag.value,
-    noteDesc = descTag.value;
-
-  if (noteTitle && noteDesc) {
-    let noteInfo = {
-      title: noteTitle,
-      description: noteDesc,
-    };
-    notes.push(noteInfo);
-    localStorage.setItem("notes", JSON.stringify(notes));
-  }
-  showNotes();
-});
+let EditOverlay = DOMHandler(".js-edit-view");
---
+++ scripts/store.js
@@ -0,0 +1,110 @@
+const Store = (function () {
+  const idGenerator = (() => {
+    let id = 0;
+
+    return {
+      next: () => ++id,
+    };
+  })();
+
+  const moveObj = (objID, fromArray, toArray) => {
+    const obj = fromArray.find((obj) => obj.id == objID);
+
+    fromArray.splice(fromArray.indexOf(obj), 1);
+    toArray.push(obj);
+  };
+
+  let initialCards = [
+    {
+      id: idGenerator.next(),
+      title: "Note 1",
+      description: "Content 1",
+      class: "yellow-100-bg",
+      pin: true,
+    },
+    {
+      id: idGenerator.next(),
+      title: "Note 2",
+      description: "Content 2",
+      class: "red-100-bg",
+      pin: false,
+    },
+    {
+      id: idGenerator.next(),
+      title: "Note 3",
+      description: "Content 3",
+      class: "cyan-100-bg",
+      pin: false,
+    },
+  ];
+
+  /*********************** */
+  let trashCards = [
+    {
+      id: idGenerator.next(),
+      title: "Note 4",
+      description: "Content 4",
+      class: "pink-100-bg",
+      pin: true,
+    },
+    {
+      id: idGenerator.next(),
+      title: "Note 5",
+      description: "Content 5",
+      class: "blue-200-bg",
+      pin: false,
+    },
+  ];
+  /*********************** */
+
+  return {
+    cards: JSON.parse(localStorage.getItem("cards")) || initialCards,
+    trashCards: JSON.parse(localStorage.getItem("trashCards")) || trashCards,
+    createCard(card) {
+      card.id = idGenerator.next();
+      this.cards.push(card);
+
+      localStorage.setItem("cards", JSON.stringify(this.cards));
+    },
+    editCard(id, editedCard, classFF) {
+      let card = this.cards.find((card) => card.id == id);
+      card.title = editedCard.title;
+      card.description = editedCard.description;
+      card.class = classFF;
+
+      localStorage.setItem("cards", JSON.stringify(this.cards));
+    },
+    trashCard(id) {
+      moveObj(id, this.cards, this.trashCards);
+
+      localStorage.setItem("cards", JSON.stringify(this.cards));
+      localStorage.setItem("trashCards", JSON.stringify(this.trashCards));
+    },
+    restoreCard(id) {
+      moveObj(id, this.trashCards, this.cards);
+
+      localStorage.setItem("cards", JSON.stringify(this.cards));
+      localStorage.setItem("trashCards", JSON.stringify(this.trashCards));
+    },
+    deleteCard(id) {
+      const index = this.trashCards.findIndex(
+        (trashCard) => trashCard.id == id
+      );
+      this.trashCards.splice(index, 1);
+
+      localStorage.setItem("trashCards", JSON.stringify(this.trashCards));
+    },
+    changeColor(id, newClass) {
+      const card = this.cards.find((card) => card.id == id);
+      card.class = newClass;
+
+      localStorage.setItem("cards", JSON.stringify(this.cards));
+    },
+    togglePin(id) {
+      const card = this.cards.find((card) => card.id == id);
+      card.pin = !card.pin;
+
+      localStorage.setItem("cards", JSON.stringify(this.cards));
+    },
+  };
+})();
